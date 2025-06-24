3f7288a7dcf3f644a9d21956fbe513ecdf0e2824
Jalal Kalyati <jalal.kalyati@gmail.com>
2025-06-24 16:06:04 -0400

    Create FastAPI app with POST /prediction endpoint

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import joblib
+import numpy as np
+import pandas as pd
+
+model = joblib.load("./model/notebooks/svc_best_model.pkl")
+df = pd.read_csv("./model/data/Iris.csv", index_col="Id")
+_, uniques = pd.factorize(df["Species"])
+
+
+class IrisFeatures(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
+app = FastAPI()
+
+
+@app.post("/prediction")
+async def predict(features: IrisFeatures):
+    X = pd.DataFrame(
+        {
+            "SepalLengthCm": [features.sepal_length],
+            "SepalWidthCm": [features.sepal_width],
+            "PetalLengthCm": [features.petal_length],
+            "PetalWidthCm": [features.petal_width],
+        }
+    )
+    species = uniques[model.predict(X)]
+    return {"species": species[0].split("-")[1].title()}
